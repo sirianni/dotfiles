48a16fa5ff27899d5c95d2250dfbffa589b4fb4e
Eric Sirianni <eric.sirianni@gmail.com>
2017-06-19 10:38:29 -0400

    Enhanced prompt for GIT repos.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,20 +7,30 @@ precmd() {
 }
 
 autoload -Uz vcs_info
-zstyle ':vcs_info:*' enable hg git
+zstyle ':vcs_info:*' enable hg
 zstyle ':vcs_info:*' disable-patterns "$HOME/rtphome(|/*)"
 zstyle ':vcs_info:hg:*' hgrevformat "%r:%h"
 zstyle ':vcs_info:hg:*' branchformat "%b"
-zstyle ':vcs_info:hg:*' get-revision true
-zstyle ':vcs_info:hg:*' check-for-changes true
-zstyle ':vcs_info:hg:*' unstagedstr "*"
+zstyle ':vcs_info:*:*' get-revision true
+zstyle ':vcs_info:*:*' check-for-changes true
+zstyle ':vcs_info:*:*' unstagedstr "☆"
+zstyle ':vcs_info:*:*' stagedstr "★"
+# hg support* 2014-Dec 4760:7bcff90ab54a
 zstyle ':vcs_info:hg:*' formats "%{$fg[grey]%}%s %{$fg[white]%}%r%{$fg[red]%}%u %{$fg_bold[yellow]%}%b%{$reset_color%} %{$fg[blue]%}%i%{$reset_color%}%{$reset_color%}"
 
-# hg support* 2014-Dec 4760:7bcff90ab54a
+# zstyle ':vcs_info:git:*' get-revision true
+# zstyle ':vcs_info:git:*' check-for-changes true
+# zstyle ':vcs_info:git:*' check-for-unstaged-changes true
+
+# Using https://github.com/olivierverdier/zsh-git-prompt instead of native vsc info
+# zstyle ':vcs_info:git:*' formats "%{$fg[grey]%}%s %{$fg[white]%}%r%{$fg[red]%}%u%c %{$fg_bold[yellow]%}%b%{$reset_color%} %{$fg[blue]%}%i%{$reset_color%}%{$reset_color%}"
+
+# [esirianni@esirianni-ub ~/src/ansible]
+# $
 export PROMPT='[%{$reset_color$fg[cyan]%}%n%{$reset_color%}@%{$fg_bold[blue]%}%m%{$reset_color%} %{$bold_color$fg[cyan]%}%~%{$reset_color%}]$prompt_newline%{$fg_bold[yellow]%}$%{$reset_color%} '
 
 # Align RPROMPT to first line of PROMPT
 # http://superuser.com/questions/357107/zsh-right-justify-in-ps1
 _lineup=$'\e[1A'
 _linedown=$'\e[1B'
-export RPROMPT='%{${_lineup}%}${vcs_info_msg_0_}%{${_linedown}%}'
+export RPROMPT='%{${_lineup}%}$(git_super_status)${vcs_info_msg_0_}%{${_linedown}%}'
