5b1c5dd985bad42f7d59bbedd601c0faf953ea4f
Eric Sirianni <eric.sirianni@hpe.com>
2018-04-11 11:22:03 -0400

    Updates.

--- nimble/nimble.zsh
+++ nimble/nimble.zsh
@@ -1,14 +1,9 @@
-export MAVEN_HOME=/opt/apache-maven-3.3.9
+export MAVEN_HOME=/opt/maven
 export PATH=$PATH:$MAVEN_HOME/bin
-
-export ANT_HOME=/opt/apache-ant-1.9.4
-export PATH=$PATH:$ANT_HOME/bin
-
-export GRADLE_HOME=/opt/gradle-3.1
-export PATH=$PATH:$GRADLE_HOME/bin
 
 hash -d infosight=/home/esirianni/src/infosight
 hash -d portal=~infosight/portal
+hash -d stairway=~/go/src/infosight/stairway
 
 alias pg='psql -h localhost -d portaldb'
 
