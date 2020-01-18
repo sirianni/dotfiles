51454a652f3d0981738a3196c2ec8c795cd6a66b
Eric Sirianni <eric.sirianni@gmail.com>
2020-01-18 15:51:59 -0500

    Updates

--- confluent/confluent.zsh
+++ confluent/confluent.zsh
@@ -3,6 +3,13 @@ hash -d ccloud=$HOME/go/src/github.com/confluentinc
 
 alias avro='java -jar /opt/avro/avro-tools-1.8.2.jar'
 
+cdpath=(. $HOME/src/confluentinc $HOME/go/src/github.com/confluentinc)
+
+ONELOGIN_DEVICE_ID=981044
+OKTA_DEVICE_ID=uft126jr2ldaPMiqZ357
+OKTA_LASTPASS=true
+
+CC_DOTFILES_BETA=true
 source ~/src/confluentinc/cc-dotfiles/caas.sh
 
-cdpath=(. $HOME/src/confluentinc $HOME/go/src/github.com/confluentinc)
+PREFERRED_BASTION=0
--- java/aliases.zsh
+++ java/aliases.zsh
@@ -1,2 +1,2 @@
-alias mvn='mvn --threads 1C'
+#alias mvn='mvn --threads 1C'
 alias jps='jps -l'
